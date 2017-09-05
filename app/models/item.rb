5d8fa749cadfd81d20d96495947555735140acf8
Melinda Fernandes <melindafernandes1993@gmail.com>
2017-09-05 16:34:44 +0100

    deleting items now deletes all associated logs

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
 	
-  has_many :logs
+  has_many :logs, dependent: :destroy
   has_one :current, -> { where return_date: nil }, class_name: "Log"
 
   attr_accessor :borrower, :lender
